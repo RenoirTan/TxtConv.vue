bfb31ba16f503a3e0c96fb47070ac3508564ef05
RenoirTan <renoirtan2005@gmail.com>
2023-09-15 00:38:15 +0800

    main: i will do this tomorrow

--- src/components/BigTitle.vue
+++ src/components/BigTitle.vue
@@ -1,9 +1,15 @@
 <template>
-  <h1>TxtConv</h1>
-  <p class="desc">Convert some text!</p>
-  <p class="disclaimer">IDK what I'm doing.</p>
+  <div class="bigtitle">
+    <h1>TxtConv</h1>
+    <p class="desc">Convert some text!</p>
+    <p class="disclaimer">IDK what I'm doing.</p>
+  </div>
 </template>
 
 <style scoped>
-  
+  .bigtitle {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
 </style>
