f1e5d8cb83b5709112513d7d694be852b7da6d7c
RenoirTan <renoirtan2005@gmail.com>
2023-09-15 22:32:57 +0800

    main: align stuff

--- src/components/BigTitle.vue
+++ src/components/BigTitle.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bigtitle">
-    <h1>TxtConv</h1>
-    <p class="desc">Convert some text!</p>
-    <p class="disclaimer">IDK what I'm doing.</p>
+    <h1 class="bigtitle-items">TxtConv</h1>
+    <p class="desc bigtitle-items">Convert some text!</p>
+    <p class="disclaimer bigtitle-items">IDK what I'm doing.</p>
   </div>
 </template>
 
@@ -11,5 +11,10 @@
     display: flex;
     flex-direction: column;
     justify-content: center;
+  }
+
+  .bigtitle-items {
+    text-align: center;
+    align-items: center;
   }
 </style>
