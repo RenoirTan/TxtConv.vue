dae40436adc380432eecb433eca3f0d7cb5e21e6
RenoirTan <renoirtan2005@gmail.com>
2023-09-15 22:49:53 +0800

    main: rearrange items

--- src/App.vue
+++ src/App.vue
@@ -25,8 +25,8 @@ header {
 @media (min-width: 1024px) {
   header {
     display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    justify-content: center;
+    /* padding-right: calc(var(--section-gap) / 2); */
   }
 
   .logo {
