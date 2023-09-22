667e509d268747780fc4ede592d1e359f1544a5a
RenoirTan <renoirtan2005@gmail.com>
2023-09-22 13:21:25 +0800

    vuetify: idk what is happening

--- src/components/TextBox.vue
+++ src/components/TextBox.vue
@@ -1,12 +1,24 @@
 <script setup>
-defineProps(["modelValue"]);
+defineProps(["modelValue", "label"]);
 defineEmits(["update:modelValue", "changed"]);
 </script>
 
 <template>
-  <input
+  <v-textarea
     type="text"
     :value="modelValue"
     @input="$emit('update:modelValue', $event.target.value); $emit('changed')"
-  />
+    :label="label"
+    variant="default"
+    auto-grow
+    class="mytextbox"
+  >
+  </v-textarea>
 </template>
+
+<style scoped>
+.mytextbox {
+  padding: 0;
+  border-bottom: 2px solid green;
+}
+</style>
